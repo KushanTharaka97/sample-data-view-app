95d4c5b4c08ab3f8c2a83a37d7208d9775b6f186
KushanTharaka <kushan.tharaka@axiatadigitallabs.com>
2024-12-27 14:35:43 +0530

    updated the add methods

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -21,7 +21,8 @@ const Add = () => {
   const handleClick = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:8800/books", book);
+      const response =  await axios.post("http://localhost:8800/books", book);
+      console.log("Book creation successful:", response.data); // Log response data
       navigate("/");
     } catch (err) {
       console.log(err);
