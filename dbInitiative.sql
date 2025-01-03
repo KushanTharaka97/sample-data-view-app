0a837a1d660291abc8f0d642b256473aecca83ed
KushanTharaka <kushan.tharaka@axiatadigitallabs.com>
2025-01-03 16:35:52 +0530

    updated the dbInitials.sql

--- dbInitiative.sql
+++ dbInitiative.sql
@@ -30,3 +30,7 @@ docker push sasithdinu/kushan_client:latest
 
 Jenkins
 docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+
+
+Password:
+cat /var/jenkins_home/secrets/initialAdminPassword
